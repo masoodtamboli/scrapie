b89bd463093d62ca90da992be5616b38be96c998
Masood Ismail Tamboli <masud.22020190@viit.ac.in>
2021-12-09 23:04:14 +0530

    Analysis

--- lib/Screens/Analyze.dart
+++ lib/Screens/Analyze.dart
@@ -7,6 +7,7 @@ class Analyze extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
     return Scaffold(
       appBar: AppBar(
         title: Text(
@@ -16,34 +17,72 @@ class Analyze extends StatelessWidget {
         backgroundColor: primaryColor,
       ),
       body: SingleChildScrollView(
-        child: AspectRatio(
-          aspectRatio: 2,
-          child: BarChart(
-            BarChartData(
-              barGroups: [
-                BarChartGroupData(
-                  x: 1,
-                  barRods: [
-                    BarChartRodData(y: 10),
-                  ],
-                ),
-                BarChartGroupData(
-                  x: 2,
-                  barRods: [
-                    BarChartRodData(y: 4),
-                  ],
-                ),
-                BarChartGroupData(
-                  x: 3,
-                  barRods: [
-                    BarChartRodData(y: 3),
-                  ],
-                ),
-              ],
-            ),
-          ),
-        ),
-      ),
+          child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Column(
+                children: [
+                  SizedBox(height: 70),
+                  Container(
+                    width: size.width / 3,
+                    height: 100,
+                    color: Colors.red,
+                  ),
+                ],
+              ),
+              Column(
+                children: [
+                  Container(
+                    width: 100,
+                    height: 100,
+                    color: Colors.red,
+                  ),
+                ],
+              ),
+              Column(
+                children: [
+                  SizedBox(height: 70),
+                  Container(
+                    width: 100,
+                    height: 100,
+                    color: Colors.red,
+                  ),
+                ],
+              ),
+            ],
+          )
+        ],
+      )),
     );
   }
 }
+
+// AspectRatio(
+//           aspectRatio: 2,
+//           child: BarChart(
+//             BarChartData(
+//               barGroups: [
+//                 BarChartGroupData(
+//                   x: 1,
+//                   barRods: [
+//                     BarChartRodData(y: 10),
+//                   ],
+//                 ),
+//                 BarChartGroupData(
+//                   x: 2,
+//                   barRods: [
+//                     BarChartRodData(y: 4),
+//                   ],
+//                 ),
+//                 BarChartGroupData(
+//                   x: 3,
+//                   barRods: [
+//                     BarChartRodData(y: 3),
+//                   ],
+//                 ),
+//               ],
+//             ),
+//           ),
+//         ),
